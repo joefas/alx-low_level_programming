468f2eaaea69c23639acb39e9988921d158335b7
joefas <ochienggeofrey06@gmail.com>
2023-09-09 07:21:47 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdi.h>
+
+/**
+ * main - print "Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
