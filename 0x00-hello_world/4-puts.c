19d90009edd630a95a8700c96f3a8757b08ff9a5
joefas <ochienggeofrey06@gmail.com>
2023-09-09 07:33:43 -0700

    updated hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
-#include <stdi.h>
+#include <stdio.h>
 
 /**
-* main - print "Programming is like building a multilingual puzzle
-*
-* Return: Always 0
-*/
+ * main - print "Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
+	return (0);
+
 }
