2d5cb6be1fd7262c1aa334da0864eac2567ec4ec
joefas <ochienggeofrey06@gmail.com>
2023-09-14 06:44:48 -0700

    Learn your times table

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,16 +9,16 @@ void jack_bauer(void)
 {
 	int min, hour;
 
-	for (hour = 0; hour <= 23; hour++)
+	for (hour = 0; hour < 24; hour++)
 	{
-		for (min = 0; min <= 59; min++)
+		for (min = 0; min < 60; min++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
 			_putchar(':');
 			_putchar((min / 10) + '0');
 			_putchar((min / 10) + '0');
-			_putchar(10);
+			_putchar('\n');
 		}
 	}
 }
