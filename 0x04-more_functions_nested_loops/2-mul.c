85262324dca4ba757aa8216f88e3d5a05d07e737
joefas <ochienggeofrey06@gmail.com>
2023-09-15 19:05:48 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-mul.c -o 2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - It multiplies two integers
+ * @x: the first number to multiply
+ * @y: the second number to multiply
+ * Return: The result of multiplication between a and b
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
