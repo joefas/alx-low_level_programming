defc623c35378e8b151bfd8ff6686817410774d5
joefas <ochienggeofrey06@gmail.com>
2023-09-30 07:12:19 -0700

    libmy.a, main.h

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
+ *
+ * Return: A pointer to the resulting string.
+ */
+char *_strcat(char *dest, const char *src)
+{
+        size_t dest_len = strlen(dest);
+        size_t i;
+
+        for (i = 0; src[i] != '\0'; i++)
+                dest[dest_len + i] = src[i];
+
+        dest[dest_len + i] = '\0';
+
+        return (dest);
+}
---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strncpy - Copies a string to a destination string.
+ * @dest: The destination string.
+ * @src: The source string to be copied
+ * @n: Number of bytes to be copied from src
+ *
+ * Return: A pointer to the destination string (dest).
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+        int i = 0, srclen = 0;
+
+        while (src[i++])
+                srclen++;
+
+        for (i = 0; src[i] && i < n; i++)
+                dest[i] = src[i];
+
+        for (i = srclen; i < n; i++)
+                dest[i] = '\0';
+
+        return (dest);
+}
