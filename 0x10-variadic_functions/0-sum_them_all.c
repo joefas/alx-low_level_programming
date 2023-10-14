7a1117f5878a326d8a007889bf745ee8b9db07aa
joefas <ochienggeofrey06@gmail.com>
2023-10-13 19:42:29 -0700

    updated

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
- * sum_then_all - calculates the sum of all its parameters
+ * sum_them_all - calculates the sum of all its parameters
  * @n: number of arguments passed to the function
  *
  * Return: the resulting sum
