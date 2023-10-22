e4e446a730e2515e858c529f737a6e88453275c0
joefas <ochienggeofrey06@gmail.com>
2023-10-22 01:32:51 -0700

    updated

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,15 @@
 section .data
 hello_msg db "Hello, Holberton", 0
+fmt: db "%s", 10, 0
 
 section .text
 global main
 extern printf
 
 main:
-sub rsp, 8; align stack pointer
+sub rsp, 8 ; align stack pointer
 mov rdi, hello_msg
 call printf
 add rsp, 8; restore stack pointer
+
 ret
