7e46255bb1f633208fdd61f77c2a85d92ddf23f9
joefas <ochienggeofrey06@gmail.com>
2023-10-30 15:29:30 -0700

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 			leng++;
 	}
 
-	op = open(filename, O_CREAT | O_TRUNC, 0600);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wrt = write(op, text_content, leng);
 
 	if (op == -1 || wrt == -1)
