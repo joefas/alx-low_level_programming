bd338be6ea721b76c1bfa7365682d23604a7b03e
joefas <ochienggeofrey06@gmail.com>
2023-10-30 08:02:03 -0700

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @text_content: pointer to the string to write
+ * @filename: a pointer to the name of file to be created
+ * Return: 1 on success or -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int op, wrt, leng = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (leng = 0; text_content[leng];)
+			leng++;
+	}
+
+	op = open(filename, O_CREAT | O_TRUNC, 0600);
+	wrt = write(op, text_content, leng);
+
+	if (op == -1 || wrt == -1)
+		return (-1);
+	close(op);
+	return (1);
+
+}
